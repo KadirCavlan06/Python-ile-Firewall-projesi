2df0b2f3e74ed5a1e37baeac3860476e6dc4d48e
KadirCavlan06 <kadircavlan06@gmail.com>
2024-12-15 17:34:51 +0300

    Python ile güvenlik duvarı
    Bugün, Python kullanarak geliştirdiğim güvenlik duvarını sizlerle paylaşmak istiyorum.
    UYARI: Programı kullanabilmek için lütfen size verdiğim .py ve .json uzantılı dosyaları aynı klasöre yerleştiriniz.
      *  Program çalıştıktan sonra log çıktısı otomatik olarak bu klasöre kaydedilecektir.

---
+++ firewall.py
@@ -0,0 +1,35 @@
+from scapy.all import sniff
+import json
+
+# Engelleme kontrol fonksiyonu
+def check_rules(packet):
+    with open("rules.json", "r") as f:
+        rules = json.load(f)
+
+    if packet.haslayer("IP"):
+        src_ip = packet["IP"].src
+        dst_port = packet["TCP"].dport if packet.haslayer("TCP") else None
+        
+        # IP kontrolü
+        if src_ip in rules["blocked_ips"]:
+            log_packet(packet, "Engellendi - IP")
+            return False
+        
+        # Port kontrolü
+        if dst_port and str(dst_port) in rules["blocked_ports"]:
+            log_packet(packet, "Engellendi - PORT")
+            return False
+        
+    log_packet(packet, "İzin Verildi")
+    return True
+
+# Paket günlüğü kaydetme
+def log_packet(packet, status):
+    with open("logs/firewall.log", "a") as f:
+        f.write(f"{status}: {packet.summary()}\n")
+    print(f"{status}: {packet.summary()}")
+
+# Trafik dinleme
+def start_firewall():
+    sniff(prn=check_rules, filter="ip", store=0)
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from firewall import start_firewall
+
+if __name__ == "__main__":
+
+	print("Güvenlik Duvarı Başlatılıyor..")
+	start_firewall()
